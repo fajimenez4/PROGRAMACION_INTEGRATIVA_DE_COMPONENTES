203a2924f37554a66f606c81f64a86444c3788ea
fajimenez4 <fajimenez4@espe.edu.ec>
2025-11-11 19:32:50 -0500

    Correccion

--- PRY_CALCULADORA/public/js/BotonOperacion.js
+++ PRY_CALCULADORA/public/js/BotonOperacion.js
@@ -6,8 +6,17 @@ class BotonOperacion extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" });
 
         shadow.innerHTML = `
+            <link rel="stylesheet" href="public/vendor/bootstrap/css/bootstrap.min.css">
             <button data-value="${operacion}" class="btn btn-light w-100">${operacion}</button>
         `;
+        const boton = shadow.querySelector("button");
+        boton.addEventListener("click", () => {
+            this.dispatchEvent(new CustomEvent("operacion-click", {
+                bubbles: true,
+                composed: true,
+                detail: { value: operacion }
+            }));
+        });
     }
 }
 customElements.define("boton-operacion", BotonOperacion);
